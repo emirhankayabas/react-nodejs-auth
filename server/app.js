63c96c3d9783b46a9b1418ec3c92dad0e637d58a
Emirhan Kayabaş <92752721+emirhankayabas@users.noreply.github.com>
2024-02-11 05:36:46 +0300

    Cors Fix
    Cors hatası çözümü.

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import connectToDatabase from "./config/db.js";
+import cors from "cors";
 
 // routes
 import userRoutes from "./routers/userRoutes.js";
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.use(express.json());
 connectToDatabase();
